9aed15ad3042ba3d72d5f7dbb2430ee1b0d5183e
efford.wang <efford.wang@applovin.com>
2023-08-09 23:58:03 +0800

    add interface for parse engine

--- internal/biz/pipeline/parseengine/interface.go
+++ internal/biz/pipeline/parseengine/interface.go
@@ -2,3 +2,15 @@ package parseengine
 
 type Interface interface {
 }
+
+// IPipelineRepo the database operation interface for pipeline
+type IPipelineRepo interface {
+}
+
+// IInstanceRepo the database operation interface for pipeline instance
+type IInstanceRepo interface {
+}
+
+// INodeRepo the database operation interface for pipeline node
+type INodeRepo interface {
+}
